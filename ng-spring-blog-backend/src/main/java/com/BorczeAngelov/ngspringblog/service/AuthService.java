c8b75b9ea451d722e7a6dd1dc96e34ed3c964945
Borcze Angelov <Borcze.Angelov@yahoo.com>
2021-12-05 22:23:34 +0100

    Encode password using Spring Security

--- ng-spring-blog-backend/src/main/java/com/BorczeAngelov/ngspringblog/service/AuthService.java
+++ ng-spring-blog-backend/src/main/java/com/BorczeAngelov/ngspringblog/service/AuthService.java
@@ -5,6 +5,7 @@ import com.BorczeAngelov.ngspringblog.model.User;
 import com.BorczeAngelov.ngspringblog.repository.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,13 +14,20 @@ public class AuthService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public void signup(RegisterRequest registerRequest) {
         User user = new User();
         user.setUserName(registerRequest.getUsername());
-        user.setPassword(registerRequest.getPassword());
+        user.setPassword(encodePassword(registerRequest.getPassword()));
         user.setEmail(registerRequest.getEmail());
 
         userRepository.save(user);
+    }
+
+    private String encodePassword(String password) {
+        return passwordEncoder.encode(password);
     }
 
 }
