0fe2ac10d53e4f2dd24c248578d16bdcf19f21ff
Borcze Angelov <Borcze.Angelov@yahoo.com>
2021-12-05 18:32:13 +0100

    first commit

---
+++ ng-spring-blog-backend/src/main/java/com/BorczeAngelov/ngspringblog/NgspringblogApplication.java
@@ -0,0 +1,13 @@
+package com.BorczeAngelov.ngspringblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgspringblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NgspringblogApplication.class, args);
+	}
+
+}
---
+++ ng-spring-blog-backend/src/test/java/com/BorczeAngelov/ngspringblog/NgspringblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BorczeAngelov.ngspringblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NgspringblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
