40ffb7ce97b74ec0e4ae9e932470822be66043c3
Borcze Angelov <Borcze.Angelov@yahoo.com>
2021-12-05 21:04:18 +0100

    Implement user registration in Spring Boot

---
+++ ng-spring-blog-backend/src/main/java/com/BorczeAngelov/ngspringblog/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.BorczeAngelov.ngspringblog.repository;
+
+import com.BorczeAngelov.ngspringblog.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
