1b27eb6f8ff8053b1b1eac82f6cdd8bbb607fd22
Borcze Angelov <Borcze.Angelov@yahoo.com>
2021-12-07 21:09:52 +0100

    create register component

--- ng-spring-blog-frontend/src/app/app.module.ts
+++ ng-spring-blog-frontend/src/app/app.module.ts
@@ -4,15 +4,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterSuccessComponent } from './auth/register-success/register-success.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    RegisterComponent,
+    LoginComponent,
+    RegisterSuccessComponent
   ],
   imports: [
+    FormsModule,
+    ReactiveFormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: 'register', component: RegisterComponent }
+    ]),
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
